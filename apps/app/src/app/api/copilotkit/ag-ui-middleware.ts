2e38c44faae5f97b088b469a4845bd7a365636f9
Tyler Slaton <tyler@copilotkit.ai>
2026-03-03 15:46:59 -0500

    feat: add A2UI
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- apps/app/src/app/api/copilotkit/ag-ui-middleware.ts
+++ apps/app/src/app/api/copilotkit/ag-ui-middleware.ts
@@ -1,14 +1,16 @@
-import { MCPAppsMiddleware } from "@ag-ui/mcp-apps-middleware";
+import { A2UIMiddleware } from "@ag-ui/a2ui-middleware";
+//import { MCPAppsMiddleware } from "@ag-ui/mcp-apps-middleware";
 
 export const aguiMiddleware = [
-  new MCPAppsMiddleware({
-    mcpServers: [
-      {
-        type: "http",
-        url:
-          process.env.MCP_SERVER_URL ||"https://mcp.excalidraw.com",
-        serverId: "example_mcp_app",
-      },
-    ],
-  }),
+  // new MCPAppsMiddleware({
+  //   mcpServers: [
+  //     {
+  //       type: "http",
+  //       url:
+  //         process.env.MCP_SERVER_URL || "https://mcp.excalidraw.com",
+  //       serverId: "example_mcp_app",
+  //     },
+  //   ],
+  // }),
+  new A2UIMiddleware(),
 ];
